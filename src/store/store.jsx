e69320642f1beb90a452704472fe8eeb2dc47921
Kenildev007 <kenilgorasya6@gmail.com>
2024-02-20 13:49:47 +0530

    done

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import UserReducer from './reducer/UserReducer';
+
 export const store = configureStore({
     reducer: {
         UserReducer: UserReducer,
     },
-})  
+})
